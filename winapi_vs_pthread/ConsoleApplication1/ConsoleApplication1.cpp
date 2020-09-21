d654f61661b398c4ad52e2baa863dfb6e7d74a40
Lounarok <wulouis0511@gmail.com>
2020-09-21 23:37:10 +0800

    Upload first version without pthread.

---
+++ winapi_vs_pthread/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,60 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#define HAVE_STRUCT_TIMESPEC // pthread doesn't do time.h guard, woops
+#include <pthread.h>
+#include <windows.h>    
+
+DWORD WINAPI helloworld(LPVOID prm) {
+    std::cout << "Hello thread\n";
+    return 0;
+}
+
+int main()
+{
+    std::cout << "WINAPI create instant run\n";
+    HANDLE instantRun = CreateThread(
+        NULL,
+        0,
+        &helloworld,
+        NULL,
+        0,// immediately run it
+        NULL
+    );
+    Sleep(50);
+    std::cout << "WINAPI create instant run done\n";
+    CloseHandle(instantRun);
+
+    std::cout << "WINAPI create suspend run\n";
+    HANDLE suspendRun = CreateThread(
+        NULL,
+        0,
+        &helloworld,
+        NULL,
+        CREATE_SUSPENDED,// immediately run it
+        NULL
+    );
+    std::cout << "WINAPI resume \n";
+    ResumeThread(suspendRun);
+    Sleep(50);
+    std::cout << "WINAPI resume done \n";
+    CloseHandle(suspendRun);
+
+    // undone, test pthread int ret = pthread_create(  )
+
+
+    // Test cpu affinity if possible
+    return 0;
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
